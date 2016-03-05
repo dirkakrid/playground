6c154bdfcf4431ca4d829a0171ebb66b7ebfaea7
Wojciech Bederski <github@wuub.net>
2016-03-05 18:44:01 +0100

    threads

--- webstack/gunicorn/app.py
+++ webstack/gunicorn/app.py
@@ -1,10 +1,10 @@
-import pprint
+import time
 
 def app(environ, start_response):
-     data = pprint.pformat(environ).encode("utf-8")
+     data = b"hello"
      start_response("200 OK", [
          ("Content-Type", "text/plain"),
          ("Content-Length", str(len(data)))
      ])
-
+     # time.sleep(0.5)
      return iter([data])
